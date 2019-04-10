7ebcd6d614c8e56fee0b8c7cc92b7fe22d16fb29
serpmelon <tyn243222791@126.com>
2019-04-10 17:21:53 +0800

    官方教程第三课；前面的有点忘了，后面不上注释

---
+++ MQServer/src/main/java/com/togo/MQServer/EmitLog.java
@@ -0,0 +1,29 @@
+package com.togo.MQServer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+
+	private static final String EXCHANGE_NAME = "logs";
+
+	public static void main(String[] args) {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+
+		try (Connection connection = factory.newConnection();
+				Channel channel = connection.createChannel()) {
+
+			channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+			String message = args.length < 1 ? "info: Hello World!" : String.join(" ", args);
+			channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+
+			System.out.println(" [x] Sent '" + message + "'");
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+}
